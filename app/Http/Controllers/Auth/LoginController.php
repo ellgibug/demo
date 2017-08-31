68153c1c1a60866fe81e79114ffe50c049058d0f
Ell Gibug <oly-fomenk@yandex.ru>
2017-08-31 17:21:35 +0400

    Custom login redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,17 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+
+    protected function redirectTo()
+    {
+        if(Auth::user()->hasRole('admin')){
+            $homepage = '/home';
+        } else {
+            $homepage = '/posts';
+        }
+
+        return $redirectTo = $homepage;
+    }
 
     /**
      * Create a new controller instance.
